01421cb04d55d1c7c831ee5a2aaa27fc28b46752
Peter Marks <pmarks@signiant.com>
2022-07-06 19:21:11 -0400

    Let's try again

--- svelte.config.js
+++ svelte.config.js
@@ -12,13 +12,15 @@ export default {
 		assets: 'docs',
 		fallback: null,
 		precompress: false,
-		paths: {
-			// change below to your repo name
-			base: dev ? "" : "/tomeclicker",
-		},
+
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: "#svelte"
     }),
+
+	paths: {
+		// change below to your repo name
+		base: dev ? "" : "/tomeclicker",
+	},
 
     prerender: {
       // This can be false if you're using a fallback (i.e. SPA mode)
