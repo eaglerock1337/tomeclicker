189c12a5ce188c7b7516fd88d0dc8326840df06b
Peter Marks <pmarks@signiant.com>
2025-10-21 19:33:42 -0400

    I give up

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,9 @@
 import adapter from '@sveltejs/adapter-static';
-import { execSync } from 'node:child_process';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const dev = process.env.NODE_ENV === 'development';
 const isGitHubPages = process.env.GITHUB_PAGES === 'true';
-// eslint-disable-next-line no-unused-vars
 const isProduction = process.env.NODE_ENV === 'production' && !isGitHubPages;
-const commit =
-	process.env.GITHUB_SHA ||
-	process.env.VERCEL_GIT_COMMIT_SHA ||
-	execSync('git rev-parse --short HEAD').toString().trim();
 
 export default {
 	preprocess: vitePreprocess(),
@@ -20,9 +14,6 @@ export default {
 			fallback: null,
 			precompress: false
 		}),
-		version: {
-			name: commit
-		},
 		paths: {
 			// No base path for production cluster deployment
 			// Use /tomeclicker for GitHub Pages staging
